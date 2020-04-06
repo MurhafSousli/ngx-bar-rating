3c82a5942abd37cd3f4fc1a62b663e3be06d43db
Rainer Maucher <rainer.maucher@gmail.com>
2020-04-06 14:03:04 +0200

    change empty ratings with no value show no leveltext instead of NaN

--- src/pipe/bar-rating.pipe.ts
+++ src/pipe/bar-rating.pipe.ts
@@ -7,7 +7,7 @@ export class BarRatingPipe implements PipeTransform {
 
   transform(value: number = 0, titles?: any): any {
     /** Initialize value with 0 in case of undefined */
-    return titles[value] || value + 1;
+    return titles[value] || '';
   }
 
 }
