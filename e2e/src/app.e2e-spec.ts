9cecf71a2addfef4d523749e491284eb92639821
Murhaf Sousli <murhafsousli@gmail.com>
2022-02-04 13:03:48 +0100

    Rename demo project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('new-pro app is running!');
+    expect(page.getTitleText()).toEqual('demo app is running!');
   });
 
   afterEach(async () => {
