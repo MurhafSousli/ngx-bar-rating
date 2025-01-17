291cb98a91653141a7a1943e201f2b40c4830c0e
Murhaf Sousli <murhafsousli@gmail.com>
2025-01-17 20:56:23 +0100

    Update

--- projects/ngx-bar-rating/src/lib/bar-rating.module.ts
+++ projects/ngx-bar-rating/src/lib/bar-rating.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BarRating } from './bar-rating';
 import { ActiveRating, InactiveRating, FractionRating } from './custom-rating';
+import { BarRatingEffect } from './bar-rating-effect';
 
 @NgModule({
   imports: [
     BarRating,
+    BarRatingEffect,
     ActiveRating,
     InactiveRating,
     FractionRating
   ],
   exports: [
     BarRating,
+    BarRatingEffect,
     ActiveRating,
     InactiveRating,
     FractionRating
